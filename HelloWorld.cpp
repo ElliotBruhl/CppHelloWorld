6f200b1ca53a4a74d3a4b5935f8b65f1226d1a59
ElliotBruhl <bruhl23188@outlook.com>
2024-08-21 11:05:33 -0400

    Hello World

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
-#include <vector>
-#include <string>
-
 using namespace std;
 
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    cout << "Hello World";
 
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+    return 0;
 }
